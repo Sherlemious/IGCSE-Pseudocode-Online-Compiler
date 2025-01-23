5906b3172926eb97d289ffee254377ffe530a5b8
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2025-01-23 04:17:58 +0200

    Feat - add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Updated import for React Router
 import Home from './pages/home';
 import HowToUse from './pages/howToUse';
+import Header from './components/layout/header';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen h-full overflow-hidden">
+        <Header />
         {/* Main content */}
         <main className="flex-grow overflow-hidden">
           <Routes>
