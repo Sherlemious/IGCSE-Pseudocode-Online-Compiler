e34d8bc9452949de7d92a5466a3a7064cd68949c
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2025-01-30 17:07:38 +0200

    minor

--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -4,9 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="h-12 bg-background border-t border-dark-text">
       <div className="max-w-144 mx-auto h-full px-6 flex items-center justify-center">
-        <p className="text-sm text-dark-text">
-          © {new Date().getFullYear()} Abdelrahman Mohammed. All rights reserved.
-        </p>
+        <p className="text-sm text-dark-text">© {new Date().getFullYear()} Abdelrahman Mohammed.</p>
       </div>
     </footer>
   );
