fd703c76a7c0843d27359d9880caae588bbe4acf
Abdelrahman Mohammed <abd.moh.yousef@gmail.com>
2026-02-07 03:19:40 +0200

    remove backend

--- backend/apps/api/execution_engine/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import execute_code
-
-urlpatterns = [
-    path('convert/', execute_code, name='convert_code'),
-]
--- backend/pseudocodeCompiler/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/', include('apps.api.urls')),
-    path('execution/', include('apps.api.execution_engine.urls')),
-]
